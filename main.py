57683a0885d2ba2dc3697cc73365a88506e1ac35
James Bithell <hi@jbithell.com>
2017-09-09 20:05:25 +0100

    Test key

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(2,GPIO.OUT) #YellowLed
 GPIO.setup(17,GPIO.OUT) #RedLed
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #EmergencyStop
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Key
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP) #EmergencyStop
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Key
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Start
 
 
