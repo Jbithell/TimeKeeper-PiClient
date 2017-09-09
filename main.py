4bba92dd798c1215fb9c61f269f5dba19aa7b725
James Bithell <hi@jbithell.com>
2017-09-09 18:48:31 +0100

    Run a fuller test

--- main.py
+++ main.py
@@ -3,16 +3,25 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)
-print "LED on"
-GPIO.output(17,GPIO.HIGH)
-'''
+GPIO.setup(2,GPIO.OUT) #YellowLed
+GPIO.setup(17,GPIO.OUT) #RedLed
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP) #EmergencyStop
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Key
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Start
 
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 while True:
-input_state = GPIO.input(18)
-if input_state == False:
-    print('Stop Button Pressed')
-    time.sleep(0.2)
-'''
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(2, GPIO.HIGH)
+
+    if GPIO.input(4) == False:
+        print('Stop Button Pressed')
+    if GPIO.input(3) == False:
+        print('Key Switched')
+    if GPIO.input(27) == False:
+        print('Start Button Pressed')
+
+    GPIO.output(17, GPIO.HIGH)
+    GPIO.output(2, GPIO.LOW)
+    time.sleep(1)
