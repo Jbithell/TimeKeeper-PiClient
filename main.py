50fe2bd35484307f953d55d774115adae6c51ad6
James Bithell <hi@jbithell.com>
2017-09-09 21:33:57 +0100

    Create some basic logic to get us through the night

--- main.py
+++ main.py
@@ -7,18 +7,33 @@ GPIO.setup(2,GPIO.OUT) #YellowLed
 GPIO.setup(17,GPIO.OUT) #RedLed
 
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP) #EmergencyStop
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #Key
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Key
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Start
+
+#Starting Values
 GPIO.output(2, GPIO.HIGH)
-GPIO.output(17, GPIO.HIGH)
+GPIO.output(17, GPIO.LOW)
+
+def keyOn():
+    global GPIO
+    return GPIO.input(3)
+def startOn():
+    global GPIO
+    if GPIO.input(17) == False:
+        return True
+    else:
+        return False
+def stopOn():
+    global GPIO
+    return GPIO.input(4)
 
 while True:
-    if GPIO.input(4):
-        print('Stop Button Pressed')
-    if GPIO.input(3) == False:
-        print('Key Switched')
-    if GPIO.input(27) == False:
-        print('Start Button Pressed')
-
-
-    time.sleep(1)
+    if keyOn():
+        GPIO.output(2, GPIO.HIGH)
+        #Main program logic
+        if stopOn():
+            GPIO.output(17, GPIO.HIGH)
+    else:
+        #System Shut Down
+        GPIO.output(2, GPIO.LOW)
+        GPIO.output(17, GPIO.LOW)
