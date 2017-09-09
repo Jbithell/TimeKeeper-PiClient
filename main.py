15736dc00f284059fc135e85991151f1c55bc53f
James Bithell <hi@jbithell.com>
2017-09-09 18:39:02 +0100

    Try an example led

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+print "LED on"
+GPIO.output(17,GPIO.HIGH)
+'''
 
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(18)
-    if input_state == False:
-        print('Stop Button Pressed')
-        time.sleep(0.2)
+input_state = GPIO.input(18)
+if input_state == False:
+    print('Stop Button Pressed')
+    time.sleep(0.2)
+'''
