a226fddad32c57fc85d19291ca755a1642859711
James Bithell <hi@jbithell.com>
2018-01-05 22:09:08 +0000

    Try again

--- src/main.py
+++ src/main.py
@@ -1,1 +1,48 @@
-print("Hi")
+import termios, fcntl, sys, os
+import time
+import serial
+fd = sys.stdin.fileno()  # Start system to gather text
+
+print "Waking LCD"
+port = serial.Serial(
+    "/dev/ttyUSB0",
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    writeTimeout = 0,
+    timeout = 10,
+    rtscts=False,
+    dsrdtr=False,
+    xonxoff=False)
+print(port.isOpen())
+print("LCD Port Opened - Arduino Starting Up")
+for i in range(1,11): #Wait 11 seconds
+    print(str(11-i) + " seconds remaining")
+    time.sleep(1)
+print("Arduino Started Up")
+
+def lcdprint(text):
+    global port
+    port.write(str(text) + "$")
+    print("Data sent to LCD: " + str(text))
+print ("LCD AWAKE")
+lcdprint("TEST TEST TEST")
+try:
+
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+    while 1:
+        try:
+            c = sys.stdin.read(1)
+            print "Got character", repr(c)
+        except IOError: pass
+finally:
+    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
