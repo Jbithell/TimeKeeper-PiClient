0701153dd48a67cb98398e2887e9891d11923b87
James Bithell <hi@jbithell.com>
2018-01-05 22:29:13 +0000

    Stop it looping

--- src/main.py
+++ src/main.py
@@ -28,26 +28,6 @@ def lcdprint(text):
     print("Data sent to LCD: " + str(text))
 print ("LCD AWAKE")
 lcdprint("TEST TEST TEST")
-
 print("Starting text grabber")
-oldterm = termios.tcgetattr(fd)
-newattr = termios.tcgetattr(fd)
-newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
-termios.tcsetattr(fd, termios.TCSANOW, newattr)
-
-oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
-fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
-
-try:
-
-
-
-    while 1:
-        try:
-            c = sys.stdin.read(1)
-            print "Got character", repr(c)
-        except IOError: pass
-finally:
-    print("Crashing out")
-    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
-    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+while True:
+    print("1")
