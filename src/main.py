9c3527d1e20b5b1d7650e8e3f2c1703d61b874b3
James Bithell <hi@jbithell.com>
2018-01-06 10:32:13 +0000

    GPIO test

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
 import termios, fcntl, sys, os
 import time
 import serial
+import RPi.GPIO as GPIO
+
+#                   Start setting up LEDs and physical components
+GPIO.setmode(GPIO.BCM)
+yellowLED = 2
+redLED = 17
+keySWITCH = 3
+startBUTTON = 27
+stopSWITCH = 4
+
+GPIO.setup(yellowLED,GPIO.OUT)
+GPIO.output(yellowLED,GPIO.LOW)
+
+GPIO.setup(redLED,GPIO.OUT)
+GPIO.output(redLED,GPIO.LOW)
+
+GPIO.setup(keySWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(startBUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(stopSWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
+#                   Start setting up system to log text
 fd = sys.stdin.fileno()  # Start system to gather text
 oldterm = termios.tcgetattr(fd)
 newattr = termios.tcgetattr(fd)
@@ -11,9 +31,9 @@ termios.tcsetattr(fd, termios.TCSANOW, newattr)
 
 oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
 fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+#                   End
 
-
-
+#                   Start setting up LCD
 print("Waking LCD")
 port = serial.Serial(
     "/dev/ttyUSB0",
@@ -37,19 +57,23 @@ def lcdprint(text):
     global port
     port.write(str(text) + "$")
     print("Data sent to LCD: " + str(text))
+lcdprint("   TIMEKEEPER    SYSTEM ONLINE ❤")
 print ("LCD AWAKE")
-lcdprint("TEST TEST TEST")
-print("Starting text grabber")
+#                      End
 
-try:
 
-    while 1:
-        try:
-            c = sys.stdin.read(1)
-            print "Got character", repr(c)
-        except IOError:
-            pass
-finally:
-    print("Crashing out")
-    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
-    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+
+while 1:
+    if (GPIO.input(kewSWITCH)):
+        GPIO.output(redLED, GPIO.HIGH)
+        GPIO.output(yellowLED, GPIO.HIGH)
+    else:
+        GPIO.output(redLED, GPIO.LOW)
+        GPIO.output(yellowLED, GPIO.LOW)
+    '''
+    try:
+        c = sys.stdin.read(1)
+        print "Got character", repr(c)
+    except IOError:
+        pass
+    '''
