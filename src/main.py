8202ac1d4ee14d8856828d42ec8e6e3d5c9dbbad
James Bithell <hi@jbithell.com>
2018-02-22 16:03:14 +0000

    Reformatting, and try and correct unfinished functions

--- src/main.py
+++ src/main.py
@@ -11,20 +11,19 @@ print(sys.version)
 GPIO.setmode(GPIO.BOARD)
 yellowLED = 3
 redLED = 11
-keySWITCH = 5 #Reversed
-startBUTTON = 13 #Reversed
+keySWITCH = 5  # Reversed
+startBUTTON = 13  # Reversed
 stopSWITCH = 7
 
-GPIO.setup(yellowLED,GPIO.OUT)
-GPIO.output(yellowLED,GPIO.LOW)
+GPIO.setup(yellowLED, GPIO.OUT)
+GPIO.output(yellowLED, GPIO.LOW)
 
-GPIO.setup(redLED,GPIO.OUT)
-GPIO.output(redLED,GPIO.LOW)
+GPIO.setup(redLED, GPIO.OUT)
+GPIO.output(redLED, GPIO.LOW)
 
 GPIO.setup(keySWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(startBUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(stopSWITCH, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 
 #                   Start setting up system to log text
 fd = sys.stdin.fileno()  # Start system to gather text
@@ -35,10 +34,14 @@ termios.tcsetattr(fd, termios.TCSANOW, newattr)
 
 oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
 fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+
 #                   End
 #                   Start setting up RFID logger
-def signalEndReadFunction(signal,frame):
+def signalEndReadFunction(signal, frame):
     GPIO.cleanup()
+
+
 signal.signal(signal.SIGINT, signalEndReadFunction)
 MIFAREReader = MFRC522.MFRC522()
 #                   End
@@ -50,53 +53,77 @@ port = serial.Serial(
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
-    writeTimeout = 0,
-    timeout = 10,
+    writeTimeout=0,
+    timeout=10,
     rtscts=False,
     dsrdtr=False,
     xonxoff=False)
 print(port.isOpen())
 print("LCD Port Opened - Arduino Starting Up")
-for i in range(1,11): #Wait 11 seconds
-    print(str(11-i) + " seconds remaining")
+for i in range(1, 11):  # Wait 11 seconds
+    print(str(11 - i) + " seconds remaining")
     time.sleep(1)
 print("Arduino Started Up")
+
 
 def lcdprint(text):
     global port
     port.write(str(text) + "$")
-    #print("Data sent to LCD: " + str(text))
+    # print("Data sent to LCD: " + str(text))
+
+
 lcdprint("   TIMEKEEPER      SYSTEM ONLINE")
-print ("LCD AWAKE")
+print("LCD AWAKE")
 #                   End
 
 #                   Start setting up data connection
 sessionRunning = False
 sessionTimer = 0
-sessionLastStart = 0 #Last time a session was started
+sessionLastStart = 0  # Last time a session was started
 sessionTimerRunning = False
 sessionData = []
+
+
 def webRequest(path, paramstring):
     if (len(paramstring) > 0):
         paramstring = "?" + paramstring + "&"
     else:
         paramstring = "?"
-    url = "https://" + "jbithell.com/projects/timekeeper/api/v4/" + path + paramstring + "USERKEY=" + str(os.getenv('USERKEY', '')) + "&USERSECRET=" + str(os.getenv('USERSECRET', ''))
+    url = "https://" + "jbithell.com/projects/timekeeper/api/v4/" + path + paramstring + "USERKEY=" + str(
+        os.getenv('USERKEY', '')) + "&USERSECRET=" + str(os.getenv('USERSECRET', ''))
     print(url)
     response = urllib.urlopen(url)
     data = json.loads(response.read())
     return data
+
+
 def endSession():
-    global sessionRunning, sessionTimer, sessionLastStart, sessionTimerRunning
+    global sessionRunning, sessionTimer, sessionLastStart, sessionTimerRunning, sessionData
     if sessionTimerRunning:
         sessionTimer = time.time() - sessionLastStart
         sessionTimerRunning = False
         sessionLastStart = 0
         GPIO.output(redLED, GPIO.LOW)
     sessionRunning = False
-    #Session timer now acuratley represents the time for this session
-    request = webRequest("projects/get/", "id=" + str(projectID) + "&time=" + str(sessionTimer))
+    # Session timer now accurately represents the time for this session
+    request = webRequest("projects/get/", "id=" + str(sessionData[0]) + "&time=" + str(sessionTimer))
     sessionTimer = 0
+    sessionData = False
+    sessionRunning = False
+    if request["result"]:
+        return True
+    else:
+        return False
+
+    return True
+    # End a session if theres on running
+
+
+def getSessionData(projectID, cardReadID):
+    if cardReadID == False:
+        request = webRequest("projects/get/", "id=" + str(projectID))
+    elif projectID == False:
+        request = webRequest("projects/get/", "RFIDid=" + str(cardReadID))
     if request["result"]:
         # Param 1: projectid, 2: 16 characters of title (exactly 16 - pad with spaces if less), 3 is the number of seconds the project currently has a its total
         if len(request["result"]["NAME"]) > 16:
@@ -110,33 +137,16 @@ def endSession():
         return False
 
 
-    return True
-    #End a session if theres on running
-def getSessionData(projectID, cardReadID):
-    if cardReadID == False:
-        request = webRequest("projects/get/", "id=" + str(projectID))
-    elif projectID == False:
-        request = webRequest("projects/get/", "RFIDid=" + str(cardReadID))
-    if request["result"]:
-        # Param 1: projectid, 2: 16 characters of title (exactly 16 - pad with spaces if less), 3 is the number of seconds the project currently has a its total
-        if len(request["result"]["NAME"]) > 16:
-            request["result"]["NAME"] = request["result"]["NAME"][0:16]
-        elif len(request["result"]["NAME"]) < 16:
-            for x in range(0, 16-len(request["result"]["NAME"])):
-                request["result"]["NAME"] = str(request["result"]["NAME"]) + " " #Add spaces
-
-        return [request["result"]["ID"], request["result"]["NAME"], request["result"]["TOTALTIME"]]
-    else:
-        return False
-
 def is_int(input):
     try:
         num = int(input)
     except ValueError:
         return False
     return True
+
+
 def hoursMinutesSeconds(input):
-    #https://stackoverflow.com/questions/775049/python-time-seconds-to-hms
+    # https://stackoverflow.com/questions/775049/python-time-seconds-to-hms
     m, s = divmod(input, 60)
     h, m = divmod(m, 60)
 
@@ -152,35 +162,37 @@ def hoursMinutesSeconds(input):
         s = "00"
 
     if (h < 10):
-        h = "0" + str(h) #Pad
+        h = "0" + str(h)  # Pad
     if (m < 10):
-        m = "0" + str(m) #Pad
+        m = "0" + str(m)  # Pad
     if (s < 10):
         s = "0" + str(s)  # Pad
-    return (str(h),str(m),str(s))
+    return (str(h), str(m), str(s))
+
+
 #                   End
 
 #                   Start main loop
-currentMode = 0 #powered off
+currentMode = 0  # powered off
 while True:
-    if GPIO.input(keySWITCH): #System is powered down
-        if currentMode != 0: #If it's doing anything else power it down
+    if GPIO.input(keySWITCH):  # System is powered down
+        if currentMode != 0:  # If it's doing anything else power it down
             print("Powering down")
-            time.sleep(0.1) #Big old debounce
-            #System has literally just been powered down on this iteration of main loop
+            time.sleep(0.1)  # Big old debounce
+            # System has literally just been powered down on this iteration of main loop
             GPIO.output(redLED, GPIO.LOW)
             GPIO.output(yellowLED, GPIO.LOW)
             if sessionRunning:
-                endSession() #Just in case you left one running when powering down
+                endSession()  # Just in case you left one running when powering down
             lcdprint("   TIMEKEEPER                 v6")
             currentMode = 0
-    elif currentMode == 0: #Keyswitch is on but system technically powered down
-        #System is getting powered on
+    elif currentMode == 0:  # Keyswitch is on but system technically powered down
+        # System is getting powered on
         print("Powering up")
         time.sleep(0.05)  # Debounce
-        GPIO.output(yellowLED, GPIO.HIGH) #this is the power indicator
+        GPIO.output(yellowLED, GPIO.HIGH)  # this is the power indicator
         lcdprint("   TIMEKEEPER   ENTER PROJECT ID")
-        currentMode = 1 #Entry menu
+        currentMode = 1  # Entry menu
         projectIDEntered = ""
         cardReadID = ""
         while True:
@@ -192,11 +204,11 @@ while True:
                     print("Got " + str(c))
                     lcdprint("   TIMEKEEPER   " + projectIDEntered)
                     time.sleep(0.01)  # Debounce
-                elif c == "\n" and len(projectIDEntered) > 0: #Can't hit enter immidiatley
+                elif c == "\n" and len(projectIDEntered) > 0:  # Can't hit enter immediately
                     break
             except IOError:
                 pass
-            #       EndKeypad
+            # EndKeypad
 
             #       RFID
             (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
@@ -205,17 +217,19 @@ while True:
                 cardReadID = (str(uid[0]) + "," + str(uid[1]) + "," + str(uid[2]) + "," + str(uid[3]))
                 print(cardReadID)
                 break
-            #       EndRFID
+            # EndRFID
 
-            if GPIO.input(keySWITCH): #Power key has been turned to off
+            if GPIO.input(keySWITCH):  # Power key has been turned to off
                 time.sleep(0.2)  # Debounce
                 print("Key switched back")
                 projectIDEntered = ""
                 cardReadID = ""
                 break
-        if GPIO.input(keySWITCH) != True and (len(projectIDEntered) > 0 or len(cardReadID) > 0): #Check we didn't break because of power switch
+        if GPIO.input(keySWITCH) != True and (
+                        len(projectIDEntered) > 0 or len(
+                    cardReadID) > 0):  # Check we didn't break because of power switch
             if len(cardReadID) > 0:
-                #They've swiped an RFID thing not actually used a project ID so we can ignore the project ID and make a slightly different request
+                # They've swiped an RFID thing not actually used a project ID so we can ignore the project ID and make a slightly different request
                 print("Loading RFID " + cardReadID)
                 lcdprint("TIMEKEEPER LOAD+" + "   RFID SWIPE   ")
                 sessionData = getSessionData(False, cardReadID)
@@ -225,33 +239,33 @@ while True:
                 sessionData = getSessionData(projectIDEntered, False)
             if sessionData:
                 lcdprint(sessionData[1] + "{}:{}:{}".format(*hoursMinutesSeconds(sessionData[2])) + "   READY")
-                currentMode = 2 #Session start/running page
+                currentMode = 2  # Session start/running page
             else:
                 currentMode = 0
     elif currentMode == 2:
         if sessionRunning:
-            #There's a session runnig so we want to check for various stuff
+            # There's a session running so we want to check for various stuff
             if GPIO.input(startBUTTON) != True:
-                #Someone wants to end the session
+                # Someone wants to end the session
                 print("Ending session")
                 time.sleep(0.2)  # Debounce
                 endSession()
                 currentMode = 0
             elif GPIO.input(stopSWITCH) and sessionTimerRunning:
-                #So we're still running the timer but it needs to be paused
+                # So we're still running the timer but it needs to be paused
                 print("Pause")
                 time.sleep(0.05)  # Debounce
-                sessionTimer += time.time()-sessionLastStart #add the time to the thing
+                sessionTimer += time.time() - sessionLastStart  # add the time to the thing
                 sessionTimerRunning = False
                 GPIO.output(redLED, GPIO.LOW)
             elif GPIO.input(stopSWITCH) != True and sessionTimerRunning != True:
-                #Need to restart the timer
+                # Need to restart the timer
                 print("Resume")
                 time.sleep(0.05)  # Debounce
                 sessionLastStart = time.time()
                 sessionTimerRunning = True
                 GPIO.output(redLED, GPIO.HIGH)
-            #Print the status of the timer with a countup
+            # Print the status of the timer with a countup
             if sessionTimerRunning:
                 sessionTimerTemp = sessionTimer + (time.time() - sessionLastStart)
                 GPIO.output(redLED, GPIO.HIGH)
@@ -259,31 +273,34 @@ while True:
                 GPIO.output(redLED, GPIO.LOW)
                 time.sleep(0.3)  # Try not to kill LCD by constantly sending it updates
             else:
-                #Timer is paused
+                # Timer is paused
                 sessionTimerTemp = sessionTimer
                 time.sleep(0.5)  # Try not to kill LCD by constantly sending it updates
 
-            lcdprint(sessionData[1] + "{}:{}".format(*hoursMinutesSeconds(sessionData[2] + sessionTimerTemp)) + "   " + "{}:{}:{}".format(*hoursMinutesSeconds(sessionTimerTemp)))
+            lcdprint(sessionData[1] + "{}:{}".format(
+                *hoursMinutesSeconds(sessionData[2] + sessionTimerTemp)) + "   " + "{}:{}:{}".format(
+                *hoursMinutesSeconds(sessionTimerTemp)))
 
 
         else:
             if GPIO.input(stopSWITCH):
-                time.sleep(0.2) #Debounce
+                time.sleep(0.2)  # Debounce
                 print("Release stop")
-                lcdprint("  RELEASE STOP  !!!!!!!!!!!!!!!!") #We can't start a session when the stop thing is pressed down
+                lcdprint(
+                    "  RELEASE STOP  !!!!!!!!!!!!!!!!")  # We can't start a session when the stop thing is pressed down
                 while GPIO.input(stopSWITCH):
-                    #Basically sit in a huge loop until either stop is released or they power down
+                    # Basically sit in a huge loop until either stop is released or they power down
                     if GPIO.input(keySWITCH):
-                        time.sleep(0.05) #Debounce
-                        #Poweroff basically
+                        time.sleep(0.05)  # Debounce
+                        # Poweroff basically
                         break
                 if GPIO.input(keySWITCH) != True:
                     time.sleep(0.5)  # Debounce
                     lcdprint(sessionData[1] + "{}:{}:{}".format(*hoursMinutesSeconds(sessionData[2])) + "   READY")
             elif GPIO.input(startBUTTON) != True:
-                #Start running session
+                # Start running session
                 print("Starting")
-                time.sleep(2)  # Debounce - this one tends to be problematic
+                time.sleep(0.5)  # Debounce - this one tends to be problematic
                 sessionLastStart = time.time()
                 sessionTimerRunning = True
                 sessionRunning = True
